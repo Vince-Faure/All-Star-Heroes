33e0bcfc9e29366db1372a32dd250ee3a54cb0ec
vinceFaure <faurvincent@gmail.com>
2023-05-29 18:05:41 +0200

    global css list component .Css

--- src/app/heroes-list/heroes-list.component.ts
+++ src/app/heroes-list/heroes-list.component.ts
@@ -12,7 +12,7 @@ export class HeroesListComponent implements OnInit {
   heroListTitle ='Heroes List';
 
   heroes: Hero[] =[
-    {id : 1 , name : 'BatMan', superPower:'Money', realName : 'Bruce Wayne', picturePath : '../assets/Batman.png'},
+    {id : 1 , name : 'Batman', superPower:'Money', realName : 'Bruce Wayne', picturePath : '../assets/Batman.png'},
     {id : 2 , name : 'Deadpool', superPower:'UnKillable', realName : 'Wayde Wilson', picturePath : '../assets/Deadpool.png'},
     {id : 3 , name : 'Storm', superPower:'Weather Witch', realName : 'Ororo Munroe', picturePath : '../assets/Storm.png'},
     {id : 4 , name : 'Punisher', superPower:'Abnormally Pain Tolerance ', realName :'Franck Castle', picturePath :'../assets/Punisher.png'},
