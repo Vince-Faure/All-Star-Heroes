1693904bad42f3804436051f5b456f8a1550ba05
vinceFaure <faurvincent@gmail.com>
2023-05-30 16:48:26 +0200

    adding character hero detail

--- src/app/heroes-list/heroes-list.component.ts
+++ src/app/heroes-list/heroes-list.component.ts
@@ -12,10 +12,10 @@ export class HeroesListComponent implements OnInit {
   heroListTitle ='Heroes List';
 
   heroes: Hero[] =[
-    {id : 1 , name : 'Batman', superPower:'Money', realName : 'Bruce Wayne', picturePath : '../assets/Batman.png'},
-    {id : 2 , name : 'Deadpool', superPower:'UnKillable', realName : 'Wayde Wilson', picturePath : '../assets/Deadpool.png'},
-    {id : 3 , name : 'Storm', superPower:'Weather Witch', realName : 'Ororo Munroe', picturePath : '../assets/Storm.png'},
-    {id : 4 , name : 'Punisher', superPower:'Abnormally Pain Tolerance ', realName :'Franck Castle', picturePath :'../assets/Punisher.png'},
+    {id : 1 , name : 'Batman', superPower:'Money', realName : 'Bruce Wayne', weakness:' Psychological Trauma',quote:'"Let that be a lesson. In the future, be more careful from who you accept free lemonade."', picturePath : '../assets/Batman.png'},
+    {id : 2 , name : 'Deadpool', superPower:'UnKillable', realName : 'Wayde Wilson',weakness:'None',quote:'Fxxxk-you!' ,picturePath : '../assets/Deadpool.png'},
+    {id : 3 , name : 'Storm', superPower:'Weather Witch', realName : 'Ororo Munroe',weakness:'Severe Claustrophobia,',quote:'Well. I am Storm, monster... and I resist you! And I will not be destroyed!' ,picturePath : '../assets/Storm.png'},
+    {id : 4 , name : 'Punisher', superPower:'Abnormally Pain Tolerance ', realName :'Franck Castle',weakness:'Finite Amount of Ammunition',quote:'Si vis pacem, para bellum' ,picturePath :'../assets/Punisher.png'},
   ]
 
   selectedHero: Hero;
--- src/app/types/hero.type.ts
+++ src/app/types/hero.type.ts
@@ -4,5 +4,7 @@ id: number ;
 name: string ;
 superPower: string ;
 realName: string ;
+weakness: string ;
+quote: string ;
 picturePath: string ;
 }
